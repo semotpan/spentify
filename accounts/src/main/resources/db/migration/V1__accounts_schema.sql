421adda95ff672a36709cf196db6fd8a6bf7520d
Serghei Motpan <semotpan@gmail.com>
2023-11-18 10:55:09 +0200

    Migrate from mysql to postgres

--- accounts/src/main/resources/db/migration/V1__accounts_schema.sql
+++ accounts/src/main/resources/db/migration/V1__accounts_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS accounts
 (
-    id            BINARY(16) PRIMARY KEY,
-    creation_date TIMESTAMP    NOT NULL DEFAULT NOW(),
+    id            UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+    creation_date TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     email_address VARCHAR(256) NOT NULL UNIQUE,
     first_name    VARCHAR(255),
     last_name     VARCHAR(255)
--- accounts/src/test/java/io/spentify/accounts/TestAccountsApplication.java
+++ accounts/src/test/java/io/spentify/accounts/TestAccountsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,8 +12,8 @@ public class TestAccountsApplication {
 
     @Bean
     @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0.33"));
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
     }
 
     public static void main(String[] args) {
