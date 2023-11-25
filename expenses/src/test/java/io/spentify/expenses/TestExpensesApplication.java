7829f882071233999469ecdb620972fb3502f4fb
Serghei Motpan <semotpan@gmail.com>
2023-11-25 19:40:36 +0200

    Expenses service init with github-action CI file

---
+++ expenses/src/test/java/io/spentify/expenses/TestExpensesApplication.java
@@ -0,0 +1,23 @@
+package io.spentify.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestExpensesApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ExpensesApplication::main).with(TestExpensesApplication.class).run(args);
+	}
+
+}
