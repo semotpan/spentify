6c76fe7e3d1edb708f50398b1d28dd75d154e428
Serghei Motpan <semotpan@gmail.com>
2023-11-25 19:40:36 +0200

    Expenses service init with github-action CI file

---
+++ expenses/src/test/java/io/spentify/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spentify.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TestExpensesApplication.class)
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
